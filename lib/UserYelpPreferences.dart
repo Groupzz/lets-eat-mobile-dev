7c59570b306d97456e980f5d1ba5fbcd2070f388
MCSReverb <haydenh2o@yahoo.com>
2019-11-18 01:28:49 -0800

    Fixed updatePref

--- lib/UserYelpPreferences.dart
+++ lib/UserYelpPreferences.dart
@@ -28,6 +28,8 @@ class UserYelpPreferences extends StatelessWidget{
     final String uid = user.uid;
 
     cuisineURL = parseCuisine();
+    dietURL = parseDiet();
+    priceURL = parsePrice();
 
     try {
       Firestore.instance
@@ -40,19 +42,17 @@ class UserYelpPreferences extends StatelessWidget{
       print("Could not find user");
     }
 
-
-//      document.reference.updateData({
-//        'userCuisinePref': document['userCuisinePref'] + userCuisinePref,
-//      });
-
-    //pass string to firebase
   }
   
   Future<void> updatePref(QuerySnapshot snap)async{
     Firestore.instance
         .collection('users')
-        .document(snap.documents[0]['userCuisinePref'])
-        .updateData({'userCuisinePref':cuisineURL});
+        .document(snap.documents[0].documentID)
+        .updateData({
+          'userCuisinePref':cuisineURL,
+          'userDietPref':dietURL,
+          'userPricePref':priceURL
+          });
   }
   
   String parsePrice(){ //Converts dollar signs to string ints and return URL ready string
@@ -80,7 +80,7 @@ class UserYelpPreferences extends StatelessWidget{
       }
     }
     userPricePref = userPricePrefTemp;
-    String temp;
+    String temp = "";
     for(int i = 0; i<userPricePref.length;i++){
       if(i==0){
         temp = temp+userPricePref[i];
@@ -107,7 +107,7 @@ class UserYelpPreferences extends StatelessWidget{
   }
   //Converts diet list into URL ready string
   String parseDiet(){
-    String temp;
+    String temp = "";
     for(int i = 0; i<userDietPref.length;i++){
       if(i==0){
         temp = temp+userDietPref[i];
